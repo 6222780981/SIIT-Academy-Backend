2655db8aa05e2c6f32acbfe1da6f7f5026b15c64
Krimson777 <keenno2001@gmail.com>
2022-10-24 16:48:27 +0700

    Update weekRoutes.js

--- routes/weekRoutes.js
+++ routes/weekRoutes.js
@@ -5,8 +5,8 @@ const weekController = require('../controllers/weekController');
 const router = express.Router();
 
 router.route('/').get(weekController.getWeek);
-router.route('/assignment/').post(weekController.postAssignment);
 router.route('/').post(weekController.postWeek);
 router.route('/').delete(weekController.deleteWeek);
+router.route('/assignment/').post(weekController.postAssignment);
 router.route('/material/').post(weekController.postMaterial);
 module.exports = router;
